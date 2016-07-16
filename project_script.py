06de5fb530e7cc1918338d402cd081ac19764f3e
jgartman <gartman.j@husky.neu.edu>
2016-07-16 17:17:55 -0400

    html successfully parsed

--- project_script.py
+++ project_script.py
@@ -9,29 +9,28 @@ import pandas
 import unicodedata
 from bs4 import BeautifulSoup
 
-file_path = "C:\Users\Josh\Desktop\CS\NU\CS7280\hw1\us-baby-names-release-2015-12-18-00-53-48\output\StateNames.csv"
+data_path = "C:\Users\Josh\Desktop\CS\NU\CS7280\hw1\us-baby-names-release-2015-12-18-00-53-48\output\StateNames.csv"
+table_path = "./Table7_"
+table_years = [1910,1920,1930,1940,1950,1960,1970,1980,1990,2000,2010,2020]
 
-df = pandas.read_csv(file_path)
+male = [[],[],[],[],[],[],[],[],[],[],[],[]]
+female = [[],[],[],[],[],[],[],[],[],[],[],[]]
 
-soup = BeautifulSoup(open("C:\Users\Josh\Desktop\CS\data_project\Table7_1920.html"))
+df = pandas.read_csv(data_path)
 
-#parse html to get the life table and ignore the first 18 elements which are part of the table
-#header
-lt = soup.find_all("table")[1].find_all("div")[18:]
-
-#characters are in unicode format, convert to ascii
-unicodedata.normalize('NFKD', lt[66].contents[0][1:6]).encode('ascii','ignore')
-
-male = []
-
-female = []
-
-for i in range(len(lt)):
-    if i % 14 == 1:
-        male.append( float(unicodedata.normalize('NFKD',lt[i].contents[0][1:8]).encode('ascii','ignore')))
-    elif i % 14 == 8:
-        female.append( float(unicodedata.normalize('NFKD',lt[i].contents[0][1:8]).encode('ascii','ignore')))
-        
-
-
+for i in range(len(table_years)):
+    soup = BeautifulSoup(open(table_path + str(table_years[i]) + ".html"))
+    
+    #parse html to get the life table and ignore the first 18 elements which are part of the table
+    #header
+    life_table = soup.find_all("table")[1].find_all("div")[18:]
+    print table_years[i]
+    for j in range(len(life_table)):
+        #special cases for missing data
+            
+        if j % 14 == 1:
+            male[i].append( float(unicodedata.normalize('NFKD',life_table[j].contents[0][1:8]).encode('ascii','ignore')))
+        elif j % 14 == 8:
+            female[i].append( float(unicodedata.normalize('NFKD',life_table[j].contents[0][1:8]).encode('ascii','ignore')))
+    
 
